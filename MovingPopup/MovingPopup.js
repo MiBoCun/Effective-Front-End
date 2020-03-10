a32f56a099db8e0f756a545af482b4dcdf9afbbf
MiBoCun <Midadada@aliyun.com>
2020-03-10 22:02:58 +0800

    Moving Popup

---
+++ MovingPopup/MovingPopup.js
@@ -0,0 +1,22 @@
+var initLeft=undefined;
+var initTop=undefined;
+var elementPopup=undefined;
+
+window.onload=function () {
+     init();
+}
+
+function init() {
+    initLeft=Math.random()*300;
+    initTop=Math.random()*100;
+    elementPopup=document.getElementById("MovingPopup");
+    elementPopup.style.left=initLeft;
+    elementPopup.style.top=initTop;
+    setInterval("MovingPopup()",100);
+
+}
+
+function MovingPopup() {
+    elementPopup.style.left+=5;
+    elementPopup.style.top+=5;
+}
