4d28db416ea03a7000541460309c44148b4b1c15
MiBoCun <34712164+MiBoCun@users.noreply.github.com>
2020-03-30 23:50:54 +0800

    Update MovingPopup.js
    keep moving

--- MovingPopup/MovingPopup.js
+++ MovingPopup/MovingPopup.js
@@ -1,22 +1,53 @@
 var initLeft=undefined;
 var initTop=undefined;
 var elementPopup=undefined;
+var windowWidth=undefined;
+var windowHeight=undefined;
 
 window.onload=function () {
     init();
+    
 }
 
 function init() {
     initLeft=Math.random()*300;
     initTop=Math.random()*100;
-    elementPopup=document.getElementById("MovingPopup");
+    windowWidth=document.body.clientWidth;
+    windowHeight=document.body.clientHeight;
+
+    elementPopup=document.getElementById("movingPopup");
     elementPopup.style.left=initLeft+"px";
     elementPopup.style.top=initTop+"px";
-    setInterval("MovingPopup()",100);
+    setInterval("movingPopup()",200);
 
 }
 
-function MovingPopup() {
-    elementPopup.style.left+=5;
-    elementPopup.style.top+=5;
+function movingPopup() {
+	var currentLeft=elementPopup.style.left;    
+	var currentTop=elementPopup.style.top;
+	
+    var currentLeftNum=toNumber(currentLeft);
+    var currentTopNum=toNumber(currentTop);
+   
+   // console.log(currentBottom);
+
+    if((currentTopNum+100)>=windowHeight){
+       elementPopup.style.top=(currentTopNum-10)+"px";
+    }else{
+    	elementPopup.style.top=(currentTopNum+10)+"px";
+    }
+
+    if((currentLeftNum+400)>=windowWidth){
+       elementPopup.style.left=(currentLeftNum-10)+"px";
+    }else{
+    	elementPopup.style.left=(currentLeftNum+10)+"px";
+    }
+    //elementPopup.style.left=(currentLeftNum+10)+"px";
+    
+}
+
+function toNumber(cssString){
+	return parseInt(cssString.replace("px",""));
+      
+
 }
